396efdf0c23a1d8bc40cffeec45500679bf63f07
CeszP <penalozacesz@gmail.com>
2024-02-28 23:24:45 -0600

    Styles and functionality added

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,48 @@
-console.log(tempData);
+function generateDivs(data){
+    const primeCard = document.getElementById('prime--card')
+    const newDiv  = document.createElement('div')
+    
+    // PRIME FILM
+    const mayorRate = data.reduce((max, actual) => {
+      return actual.rate > max.rate ? actual : max
+    },data[0])
+
+    newDiv.classList.add('prime--container')
+    newDiv.innerHTML = `
+    <a href="#"><img src="${mayorRate.poster}" alt="" class="prime--img"></a>
+    <div class="prime--info--container">
+      <h3 class="prime--title">${mayorRate.title}</h3>
+      <label class="prime--year">${mayorRate.year}</label>
+      <h4>Director: ${mayorRate.director}</h4>
+      <h4>Duration: ${mayorRate.duration}</h4>
+      <h4>Genre: ${mayorRate.genre}</h4>
+      <div class="prime--rate--container">
+        <label class="prime--rate">${mayorRate.rate}</label> <img class="rating--star" src="./assets/rating_star.png">
+      </div>      
+    </div>
+    `
+    primeCard.appendChild(newDiv)
+    
+    // ALL FILMS
+    const container = document.getElementById('catalog--container')
+
+    data.forEach(item => {
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('card')
+
+        newDiv.innerHTML = `
+        <a href=#>
+          <img src="${item.poster}" alt="" class="card--img">
+        </a>
+        <div class="card--info--container">
+          <h3 class="card--title">${item.title}</h3><label class="card--year">${item.year}</label>
+          <h4>Director: <label>${item.director}</label></h4>
+          <h4>Duration: <label>${item.duration}<label></h4>
+          <h4>Genre: <label>${item.genre}</label></h4>
+          <label class="card--rate">${item.rate}</label><img class="rating--star" src="./assets/rating_star.png">
+        </div>
+        `
+        container.appendChild(newDiv)
+    });
+}
+generateDivs(tempData)
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -5,7 +5,7 @@ const tempData = [
     director: "James Gunn",
     duration: "2h 16min",
     genre: ["Action", "Adventure", "Comedy"],
-    rate: 7.7,
+    rate: 7.6,
     poster:
       "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg",
   },
@@ -15,7 +15,7 @@ const tempData = [
     director: "George Lucas",
     duration: "2h 1min",
     genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
-    rate: 8.7,
+    rate: 8.9,
     poster:
       "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
   },
@@ -29,4 +29,55 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  {
+    title: "Avengers: Endgame",
+    year: 2019,
+    director: "Anthony & Joe Russo",
+    duration: "2hr 61min",
+    genre: ["Action", "Adventure"],
+    rate: 8.4,
+    poster:
+      "https://m.media-amazon.com/images/I/81Ssl7tWlfL._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    title: "The Shinning",
+    year: 1980,
+    director: "Stanley Kubrick",
+    duration: "2hr 26min",
+    genre: ["Drama", "Terror"],
+    rate: 8.4,
+    poster:
+      "https://m.media-amazon.com/images/I/51g-8R5FOhL._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    title: "Nope",
+    year: 2022,
+    director: "Jordan Peele",
+    duration: "2hr 10min",
+    genre: ["Sci-Fi"],
+    rate: 6.8,
+    poster:
+      "https://m.media-amazon.com/images/I/61sXzzIVs5L.jpg",
+  },
+  {
+    title: "A clockwork orange",
+    year: 1971,
+    director: "Stanley Kubrick",
+    duration: "2hr 16min",
+    genre: ["Sci-Fi", "Black comedy"],
+    rate: 8.3,
+    poster:
+      "https://m.media-amazon.com/images/I/51XpVpQbQQL.jpg",
+  },
+  // https://m.media-amazon.com/images/I/71nQ9pOj6qL._AC_UF894,1000_QL80_.jpg
+  {
+    title: "Batman: The dark knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    duration: "2hr 32min",
+    genre: ["Action", "Crime", "Drama"],
+    rate: 9,
+    poster:
+      "https://m.media-amazon.com/images/I/71nQ9pOj6qL._AC_UF894,1000_QL80_.jpg",
+  }
 ];
